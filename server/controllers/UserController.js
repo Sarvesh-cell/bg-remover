cca5a7255bcc899180a9936c3d12bfca62130505
Sarvesh-cell <sarvesh.londhe04@gmail.com>
2025-05-29 19:56:00 +0530

    l

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -77,7 +77,7 @@ const userCredits = async (req,res) =>{
 
         const userData = await userModel.findOne({ clerkId })
 
-        res.JSON({success:true,credits: userData.creditBalance})
+        req.JSON({success:true,credits: userData.creditBalance})
         
     } catch (error) {
          console.log(error.message);
